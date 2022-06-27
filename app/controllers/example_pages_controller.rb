400944d618c10b283b820fe147d7e434f9700082
Chris Taylor <ctaylor534@gmail.com>
2022-06-27 10:47:48 -0700

    Adds some sample paths

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -3,4 +3,19 @@ class ExamplePagesController < ApplicationController
     render json: { message: "hello"}
   end
 
+  def goodbye_method
+    render json: {
+      message: "goodbye"
+    }
+  end
+
+  def array_method
+    render json: {
+      array: [{"Key" => "Value", "Key3" => "Value3"}, {"Key2" => "Value2"}]
+    }
+  end
+
+  def html_method
+    render html: ('Basic HTML, but does this really even count')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get "/goodbye_path", controller: "example_pages", action: "goodbye_method"
 
-  get "/"
+  get "/array_path", controller: "example_pages", action: "array_method"
+
+  get "html_path", controller: "example_pages", action:
+  "html_method"
 
 end
