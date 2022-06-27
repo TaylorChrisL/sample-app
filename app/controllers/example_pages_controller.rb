6801cbc7829ed517fc2fd70a6a30b8fade1a1e81
Chris Taylor <ctaylor534@gmail.com>
2022-06-27 10:05:27 -0700

    init

---
+++ app/controllers/example_pages_controller.rb
@@ -0,0 +1,6 @@
+class ExamplePagesController < ApplicationController
+  def hello_method
+    render json: { message: "hello"}
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get "/hello_path", controller: "example_pages", action: "hello_method"
+
+  get "/goodbye_path", controller: "example_pages", action: "goodbye_method"
+
+  get "/"
+
+end
